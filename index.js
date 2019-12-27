b8aa06a7a13a883a684c5080c3a352656f33c30f
Testingoutbtw <ahmedezzcs@yahoo.com>
2019-12-27 16:37:05 +0200

    Complete input and add algorithm

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var processes;
 var resources;
+var whichInput=0;
+var data;
+var alloc=[[]];
+var max = [[]];
+var available=[];
 
 //get number of inputs
 $( "#initial-submit" ).submit(function( event ) {
-    var data= $( this ).serializeArray();
+     data= $( this ).serializeArray();
+    event.preventDefault();
+    if(whichInput===0){
     processes=data[0].value;
     resources=data[1].value;
-    event.preventDefault();
     createInputs();
+    whichInput=1;
+    }else{
+        formatInput();
+        console.log(calculateNeed());
+        console.log(safeSeq());
+    }
   });
 
 
@@ -22,40 +34,173 @@ function createInputs(){
         // Append a node with a random text
         var div = document.createElement('div');
         div.classList.add('form-group');
+        div.classList.add('div-con');
         var p = document.createElement('p');
         p.innerText='Process '+i;
         p.classList.add('text-info');
         div.appendChild(p);
+
+        //Alloc div
+        var allocDiv = document.createElement('div');
+        allocDiv.classList.add('form-group');
+        // divl.classList.add('div-con');
+
+        //MAX div
+        var maxDiv = document.createElement('div');
+        maxDiv.classList.add('form-group');
+        // divr.classList.add('div-con');
+
         // Create an <input> element, set its type and name attributes
         for(j=0;j<resources;j++){
-        var label = document.createElement('label');
-        label.innerText=String.fromCharCode(65+j);
-        var input = document.createElement("input");
-        input.type = "number";
-        input.name = "r" + i;
-        input.classList.add('form-control')
-        div.appendChild(label);
-        div.appendChild(input);
+
+        //Alloc input
+        var allocLabel = document.createElement('label');
+        allocLabel.innerText=String.fromCharCode(65+j)+" Alloc";
+        var allocInput = document.createElement("input");
+        allocInput.type = "number";
+        allocInput.name = "alloc" + i;
+        allocInput.classList.add('form-control')
+
+        //MAX input
+        var maxLabel = document.createElement('label');
+        maxLabel.innerText=String.fromCharCode(65+j)+" MAX";
+        var maxInput = document.createElement("input");
+        maxInput.type = "number";
+        maxInput.name = "max" + i;
+        maxInput.classList.add('form-control')
+
+        allocDiv.appendChild(allocLabel);
+        allocDiv.appendChild(allocInput);
+        maxDiv.appendChild(maxLabel);
+        maxDiv.appendChild(maxInput);
+        div.appendChild(allocDiv);
+        div.appendChild(maxDiv);
         // Append a line break 
         }
         form.appendChild(div);
     }
 
+    var availableDiv = document.createElement('div');
+        availableDiv.classList.add('form-group');
+        availableDiv.classList.add('avail-con');
+        var p = document.createElement('p');
+            p.innerText='Availble Resources';
+            p.classList.add('text-info');
+        availableDiv.appendChild(p);
+        for(j=0;j<resources;j++){
+        var availLabel = document.createElement('label');
+        availLabel.innerText=String.fromCharCode(65+j);
+        var availInput = document.createElement("input");
+        availInput.type = "number";
+        availInput.name = "avail" + j;
+        availInput.classList.add('form-control')
+        availableDiv.appendChild(availLabel);
+        availableDiv.appendChild(availInput);
+        }
+        form.appendChild(availableDiv);
+
     var btn = document.createElement("button");
-    btn.type='submit';
-    btn.innerText='submit';
-    btn.classList.add('btn');
-    btn.classList.add('btn-primary');
-    form.appendChild(btn);
+        btn.type='submit';
+        btn.innerText='submit';
+        btn.classList.add('btn');
+        btn.classList.add('btn-primary');
+        form.appendChild(btn);
+
+
 }
 
 
+function formatInput(){
+    let swap =0;
+    let count=0;
+    let temp=[];
+    for(i=0;i<(data.length)-resources;i++){
+        // console.log(data[i].value);
+        temp.push(data[i].value);
+        count++;
+        if(count>=resources){
+            count=0;
+            if(swap==0){
+            alloc.push(temp);
+            swap=1;
+            } else{
+                max.push(temp);
+                swap=0;
+            }
+            temp=[]
+        }
+    }
+
+    available=data.slice(1).slice(-resources);
+    for(i=0;i<available.length;i++){
+        available[i]=available[i].value;
+    }
+
+}
+
+
+function calculateNeed(){
+    let need=[];
+    for (i=1;i<=processes;i++){
+        // #temp list to hold needed values before appending to the need list
+        tempList=[]
+        for(j=0;j<resources;j++){
+            tempList.push(max[i][j]-alloc[i][j]);
+        }
+        need.push(tempList)
+    }
+    return need
+
+}
+
+function safeSeq(){
+    
+
+    //Resources needed for each process
+    let need=[]
+    need=calculateNeed();
+
+    // #finished processes initialized all to false
+    let finished=[false]*processes
+    
+    // #safe seq
+    let safe=[]
+
+    // #boolean to check if any process was allocated
+    // #if no process is allocated then system is in deadlock or safesequence was found
+   var allocated = true
+
+    while(allocated){
+        allocated=false
+        for (i=1;i<=processes;i++){
+            if (finished[i] == false){
+                // #boolean run to check if needed resources are less than or equal to available
+                 var run=true
+                for(j=0;j<resources;j++){
+                    if (need[i][j] > available[j]){
+                        run=false
+                        break
+                    }
+                }
+            }
+                if (run) {
+                    allocated=true
+                    finished[i]=true
+                    safe.push(i)
+                    for(j=0;j<resources;j++){
+                        available[j]=available[j]+alloc[i][j];
+                    }
+                }
+        }
+    }
+
+    return safe
+
+}
+
 //TO DO LIST
 /*
-make button click change form ID to input-form
-Serialize new input (use while loop and take them in order)
-add actual algorithm
+FIX actual algorithm
 display output
 add navbar for no reason
-
 */
